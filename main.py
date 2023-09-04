848d8624a77c0c978e0dac6dc278ff0866aa203a
Alex Hake <alex@alexhake.com>
2023-09-03 19:01:37 -0700

    updated to main

--- main.py
+++ main.py
@@ -10,23 +10,29 @@ def process_file_read(event):
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
     print(f"{formatted_datetime}: File Read = {event.pathname}")
 
-# Initialize a WatchManager
-wm = pyinotify.WatchManager()
 
-# Define the watch mask to only monitor read events
-mask = pyinotify.IN_ACCESS
-
-# Create a Notifier object
-notifier = pyinotify.Notifier(wm, default_proc_fun=process_file_read)
-
-# Add a watch on the directory or file you want to monitor
-path_to_monitor = "/media/postdata"
-wm.add_watch(path_to_monitor, mask)
-
-while True:
-        notifier.process_events()
-        time.sleep(10)
-
-        if notifier.check_events():
-                notifier.read_events()
             
+
+def main():
+    # Initialize a WatchManager
+    wm = pyinotify.WatchManager()
+
+    # Define the watch mask to only monitor read events
+    mask = pyinotify.IN_ACCESS
+
+    # Create a Notifier object
+    notifier = pyinotify.Notifier(wm, default_proc_fun=process_file_read)
+
+    # Add a watch on the directory or file you want to monitor
+    path_to_monitor = "/media/postdata"
+    wm.add_watch(path_to_monitor, mask)
+
+    while True:
+            notifier.process_events()
+            time.sleep(10)
+
+            if notifier.check_events():
+                    notifier.read_events()
+
+if __name__ == "__main__":
+       main()
